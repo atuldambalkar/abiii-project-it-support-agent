c4267d319f3b2a06a020763cecc7f3ee12c1e072
Atul Dambalkar <atul.dambalkar@gmail.com>
2026-05-27 00:08:38 -0500

    Fix: Add OSS dependency ordering, IAM aoss:APIAccessAll, remove hardcoded bucket name

--- infra/src/main/java/com/example/itsupport/infra/KnowledgeBaseStack.java
+++ infra/src/main/java/com/example/itsupport/infra/KnowledgeBaseStack.java
@@ -14,6 +14,9 @@ import java.util.Map;
  * - OpenSearch Serverless collection (vector search)
  * - Bedrock Knowledge Base with S3 data source
  * - IAM roles for Bedrock to access S3 and OpenSearch
+ *
+ * <p>Note: As of CDK, Bedrock Knowledge Base and OpenSearch Serverless
+ * constructs use CfnResource (L1) since L2 constructs are not yet available.
  */
 public class KnowledgeBaseStack extends Stack {
 
@@ -22,16 +25,15 @@ public class KnowledgeBaseStack extends Stack {
     public KnowledgeBaseStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
-        // S3 bucket for runbook documents
+        // S3 bucket for runbook documents (auto-generated name to avoid conflicts)
         this.runbookBucket = Bucket.Builder.create(this, "RunbookBucket")
-                .bucketName("it-support-agent-runbooks-" + this.getAccount())
                 .versioned(true)
                 .encryption(BucketEncryption.S3_MANAGED)
                 .removalPolicy(RemovalPolicy.RETAIN)
                 .blockPublicAccess(BlockPublicAccess.BLOCK_ALL)
                 .build();
 
-        // IAM role for Bedrock Knowledge Base to access S3
+        // IAM role for Bedrock Knowledge Base to access S3 and OpenSearch Serverless
         Role bedrockKbRole = Role.Builder.create(this, "BedrockKBRole")
                 .roleName("it-support-bedrock-kb-role")
                 .assumedBy(new ServicePrincipal("bedrock.amazonaws.com"))
@@ -39,27 +41,23 @@ public class KnowledgeBaseStack extends Stack {
 
         runbookBucket.grantRead(bedrockKbRole);
 
-        // OpenSearch Serverless collection (vector search type)
-        CfnResource ossCollection = CfnResource.Builder.create(this, "RunbookVectorCollection")
-                .type("AWS::OpenSearchServerless::Collection")
+        // Grant the role permission to access OpenSearch Serverless
+        bedrockKbRole.addToPolicy(PolicyStatement.Builder.create()
+                .actions(List.of("aoss:APIAccessAll"))
+                .resources(List.of("arn:aws:aoss:" + this.getRegion() + ":" + this.getAccount() + ":collection/*"))
+                .build());
+
+        // OpenSearch Serverless encryption policy (MUST be created before collection)
+        CfnResource ossEncryptionPolicy = CfnResource.Builder.create(this, "OSSEncryptionPolicy")
+                .type("AWS::OpenSearchServerless::SecurityPolicy")
                 .properties(Map.of(
-                        "Name", "it-support-runbooks",
-                        "Type", "VECTORSEARCH",
-                        "Description", "Vector store for IT support runbook embeddings"
+                        "Name", "it-support-kb-encryption",
+                        "Type", "encryption",
+                        "Policy", "{\"Rules\":[{\"ResourceType\":\"collection\",\"Resource\":[\"collection/it-support-runbooks\"]}],\"AWSOwnedKey\":true}"
                 ))
                 .build();
 
-        // OpenSearch Serverless access policy
-        CfnResource ossAccessPolicy = CfnResource.Builder.create(this, "OSSAccessPolicy")
-                .type("AWS::OpenSearchServerless::AccessPolicy")
-                .properties(Map.of(
-                        "Name", "it-support-kb-access",
-                        "Type", "data",
-                        "Policy", "[{\"Rules\":[{\"ResourceType\":\"index\",\"Resource\":[\"index/it-support-runbooks/*\"],\"Permission\":[\"aoss:*\"]},{\"ResourceType\":\"collection\",\"Resource\":[\"collection/it-support-runbooks\"],\"Permission\":[\"aoss:*\"]}],\"Principal\":[\"" + bedrockKbRole.getRoleArn() + "\"]}]"
-                ))
-                .build();
-
-        // OpenSearch Serverless network policy
+        // OpenSearch Serverless network policy (MUST be created before collection)
         CfnResource ossNetworkPolicy = CfnResource.Builder.create(this, "OSSNetworkPolicy")
                 .type("AWS::OpenSearchServerless::SecurityPolicy")
                 .properties(Map.of(
@@ -69,13 +67,27 @@ public class KnowledgeBaseStack extends Stack {
                 ))
                 .build();
 
-        // OpenSearch Serverless encryption policy
-        CfnResource ossEncryptionPolicy = CfnResource.Builder.create(this, "OSSEncryptionPolicy")
-                .type("AWS::OpenSearchServerless::SecurityPolicy")
+        // OpenSearch Serverless collection (vector search type)
+        // Depends on encryption and network policies being created first
+        CfnResource ossCollection = CfnResource.Builder.create(this, "RunbookVectorCollection")
+                .type("AWS::OpenSearchServerless::Collection")
                 .properties(Map.of(
-                        "Name", "it-support-kb-encryption",
-                        "Type", "encryption",
-                        "Policy", "{\"Rules\":[{\"ResourceType\":\"collection\",\"Resource\":[\"collection/it-support-runbooks\"]}],\"AWSOwnedKey\":true}"
+                        "Name", "it-support-runbooks",
+                        "Type", "VECTORSEARCH",
+                        "Description", "Vector store for IT support runbook embeddings"
+                ))
+                .build();
+        ossCollection.addDependency(ossEncryptionPolicy);
+        ossCollection.addDependency(ossNetworkPolicy);
+
+        // OpenSearch Serverless access policy
+        // Must include both the KB role AND the bedrock service principal
+        CfnResource ossAccessPolicy = CfnResource.Builder.create(this, "OSSAccessPolicy")
+                .type("AWS::OpenSearchServerless::AccessPolicy")
+                .properties(Map.of(
+                        "Name", "it-support-kb-access",
+                        "Type", "data",
+                        "Policy", "[{\"Rules\":[{\"ResourceType\":\"index\",\"Resource\":[\"index/it-support-runbooks/*\"],\"Permission\":[\"aoss:CreateIndex\",\"aoss:UpdateIndex\",\"aoss:DescribeIndex\",\"aoss:ReadDocument\",\"aoss:WriteDocument\"]},{\"ResourceType\":\"collection\",\"Resource\":[\"collection/it-support-runbooks\"],\"Permission\":[\"aoss:CreateCollectionItems\",\"aoss:UpdateCollectionItems\",\"aoss:DescribeCollectionItems\"]}],\"Principal\":[\"" + bedrockKbRole.getRoleArn() + "\"]}]"
                 ))
                 .build();
 
@@ -107,6 +119,7 @@ public class KnowledgeBaseStack extends Stack {
                 ))
                 .build();
         knowledgeBase.addDependency(ossCollection);
+        knowledgeBase.addDependency(ossAccessPolicy);
 
         // Bedrock Knowledge Base Data Source (S3)
         CfnResource dataSource = CfnResource.Builder.create(this, "RunbookDataSource")
