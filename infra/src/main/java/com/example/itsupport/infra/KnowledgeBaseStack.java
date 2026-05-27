09d8c9ea4bbd5be5f92c578f06b9ed35ca128e79
Atul Dambalkar <atul.dambalkar@gmail.com>
2026-05-27 00:13:28 -0500

    Fix: Add Custom Resource Lambda to create vector index before KB creation

--- infra/src/main/java/com/example/itsupport/infra/KnowledgeBaseStack.java
+++ infra/src/main/java/com/example/itsupport/infra/KnowledgeBaseStack.java
@@ -2,22 +2,15 @@ package com.example.itsupport.infra;
 
 import software.amazon.awscdk.*;
 import software.amazon.awscdk.services.iam.*;
+import software.amazon.awscdk.services.lambda.Code;
+import software.amazon.awscdk.services.lambda.Function;
+import software.amazon.awscdk.services.lambda.Runtime;
 import software.amazon.awscdk.services.s3.*;
 import software.constructs.Construct;
 
 import java.util.List;
 import java.util.Map;
 
-/**
- * CDK Stack that provisions the Bedrock Knowledge Base infrastructure:
- * - S3 bucket for IT runbook documents
- * - OpenSearch Serverless collection (vector search)
- * - Bedrock Knowledge Base with S3 data source
- * - IAM roles for Bedrock to access S3 and OpenSearch
- *
- * <p>Note: As of CDK, Bedrock Knowledge Base and OpenSearch Serverless
- * constructs use CfnResource (L1) since L2 constructs are not yet available.
- */
 public class KnowledgeBaseStack extends Stack {
 
     private final Bucket runbookBucket;
@@ -25,7 +18,6 @@ public class KnowledgeBaseStack extends Stack {
     public KnowledgeBaseStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
-        // S3 bucket for runbook documents (auto-generated name to avoid conflicts)
         this.runbookBucket = Bucket.Builder.create(this, "RunbookBucket")
                 .versioned(true)
                 .encryption(BucketEncryption.S3_MANAGED)
@@ -33,129 +25,143 @@ public class KnowledgeBaseStack extends Stack {
                 .blockPublicAccess(BlockPublicAccess.BLOCK_ALL)
                 .build();
 
-        // IAM role for Bedrock Knowledge Base to access S3 and OpenSearch Serverless
         Role bedrockKbRole = Role.Builder.create(this, "BedrockKBRole")
                 .roleName("it-support-bedrock-kb-role")
                 .assumedBy(new ServicePrincipal("bedrock.amazonaws.com"))
                 .build();
 
         runbookBucket.grantRead(bedrockKbRole);
-
-        // Grant the role permission to access OpenSearch Serverless
         bedrockKbRole.addToPolicy(PolicyStatement.Builder.create()
                 .actions(List.of("aoss:APIAccessAll"))
                 .resources(List.of("arn:aws:aoss:" + this.getRegion() + ":" + this.getAccount() + ":collection/*"))
                 .build());
 
-        // OpenSearch Serverless encryption policy (MUST be created before collection)
         CfnResource ossEncryptionPolicy = CfnResource.Builder.create(this, "OSSEncryptionPolicy")
                 .type("AWS::OpenSearchServerless::SecurityPolicy")
-                .properties(Map.of(
-                        "Name", "it-support-kb-encryption",
-                        "Type", "encryption",
-                        "Policy", "{\"Rules\":[{\"ResourceType\":\"collection\",\"Resource\":[\"collection/it-support-runbooks\"]}],\"AWSOwnedKey\":true}"
-                ))
+                .properties(Map.of("Name", "it-support-kb-encryption", "Type", "encryption",
+                        "Policy", "{\"Rules\":[{\"ResourceType\":\"collection\",\"Resource\":[\"collection/it-support-runbooks\"]}],\"AWSOwnedKey\":true}"))
                 .build();
 
-        // OpenSearch Serverless network policy (MUST be created before collection)
         CfnResource ossNetworkPolicy = CfnResource.Builder.create(this, "OSSNetworkPolicy")
                 .type("AWS::OpenSearchServerless::SecurityPolicy")
-                .properties(Map.of(
-                        "Name", "it-support-kb-network",
-                        "Type", "network",
-                        "Policy", "[{\"Rules\":[{\"ResourceType\":\"collection\",\"Resource\":[\"collection/it-support-runbooks\"]},{\"ResourceType\":\"dashboard\",\"Resource\":[\"collection/it-support-runbooks\"]}],\"AllowFromPublic\":true}]"
-                ))
+                .properties(Map.of("Name", "it-support-kb-network", "Type", "network",
+                        "Policy", "[{\"Rules\":[{\"ResourceType\":\"collection\",\"Resource\":[\"collection/it-support-runbooks\"]},{\"ResourceType\":\"dashboard\",\"Resource\":[\"collection/it-support-runbooks\"]}],\"AllowFromPublic\":true}]"))
                 .build();
 
-        // OpenSearch Serverless collection (vector search type)
-        // Depends on encryption and network policies being created first
         CfnResource ossCollection = CfnResource.Builder.create(this, "RunbookVectorCollection")
                 .type("AWS::OpenSearchServerless::Collection")
-                .properties(Map.of(
-                        "Name", "it-support-runbooks",
-                        "Type", "VECTORSEARCH",
-                        "Description", "Vector store for IT support runbook embeddings"
-                ))
+                .properties(Map.of("Name", "it-support-runbooks", "Type", "VECTORSEARCH",
+                        "Description", "Vector store for IT support runbook embeddings"))
                 .build();
         ossCollection.addDependency(ossEncryptionPolicy);
         ossCollection.addDependency(ossNetworkPolicy);
 
-        // OpenSearch Serverless access policy
-        // Must include both the KB role AND the bedrock service principal
+        Role indexCreatorRole = Role.Builder.create(this, "IndexCreatorRole")
+                .assumedBy(new ServicePrincipal("lambda.amazonaws.com"))
+                .managedPolicies(List.of(ManagedPolicy.fromAwsManagedPolicyName("service-role/AWSLambdaBasicExecutionRole")))
+                .build();
+        indexCreatorRole.addToPolicy(PolicyStatement.Builder.create()
+                .actions(List.of("aoss:APIAccessAll"))
+                .resources(List.of("arn:aws:aoss:" + this.getRegion() + ":" + this.getAccount() + ":collection/*"))
+                .build());
+
         CfnResource ossAccessPolicy = CfnResource.Builder.create(this, "OSSAccessPolicy")
                 .type("AWS::OpenSearchServerless::AccessPolicy")
-                .properties(Map.of(
-                        "Name", "it-support-kb-access",
-                        "Type", "data",
-                        "Policy", "[{\"Rules\":[{\"ResourceType\":\"index\",\"Resource\":[\"index/it-support-runbooks/*\"],\"Permission\":[\"aoss:CreateIndex\",\"aoss:UpdateIndex\",\"aoss:DescribeIndex\",\"aoss:ReadDocument\",\"aoss:WriteDocument\"]},{\"ResourceType\":\"collection\",\"Resource\":[\"collection/it-support-runbooks\"],\"Permission\":[\"aoss:CreateCollectionItems\",\"aoss:UpdateCollectionItems\",\"aoss:DescribeCollectionItems\"]}],\"Principal\":[\"" + bedrockKbRole.getRoleArn() + "\"]}]"
-                ))
+                .properties(Map.of("Name", "it-support-kb-access", "Type", "data",
+                        "Policy", "[{\"Rules\":[{\"ResourceType\":\"index\",\"Resource\":[\"index/it-support-runbooks/*\"],\"Permission\":[\"aoss:CreateIndex\",\"aoss:UpdateIndex\",\"aoss:DescribeIndex\",\"aoss:ReadDocument\",\"aoss:WriteDocument\"]},{\"ResourceType\":\"collection\",\"Resource\":[\"collection/it-support-runbooks\"],\"Permission\":[\"aoss:CreateCollectionItems\",\"aoss:UpdateCollectionItems\",\"aoss:DescribeCollectionItems\"]}],\"Principal\":[\"" + bedrockKbRole.getRoleArn() + "\",\"" + indexCreatorRole.getRoleArn() + "\"]}]"))
                 .build();
 
-        // Bedrock Knowledge Base
+        Function indexCreatorFn = Function.Builder.create(this, "IndexCreatorFunction")
+                .runtime(Runtime.PYTHON_3_12)
+                .handler("index.handler")
+                .timeout(Duration.minutes(5))
+                .role(indexCreatorRole)
+                .code(Code.fromInline(getIndexCreatorCode()))
+                .build();
+
+        CfnResource createIndex = CfnResource.Builder.create(this, "CreateVectorIndex")
+                .type("AWS::CloudFormation::CustomResource")
+                .properties(Map.of(
+                        "ServiceToken", indexCreatorFn.getFunctionArn(),
+                        "CollectionEndpoint", ossCollection.getAtt("CollectionEndpoint").toString(),
+                        "IndexName", "runbook-index",
+                        "EmbeddingDimension", "1024"))
+                .build();
+        createIndex.addDependency(ossCollection);
+        createIndex.addDependency(ossAccessPolicy);
+
         CfnResource knowledgeBase = CfnResource.Builder.create(this, "ITSupportKnowledgeBase")
                 .type("AWS::Bedrock::KnowledgeBase")
                 .properties(Map.of(
                         "Name", "it-support-runbooks-kb",
-                        "Description", "Knowledge base containing IT support runbooks for grounded troubleshooting",
+                        "Description", "Knowledge base for IT support runbooks",
                         "RoleArn", bedrockKbRole.getRoleArn(),
-                        "KnowledgeBaseConfiguration", Map.of(
-                                "Type", "VECTOR",
+                        "KnowledgeBaseConfiguration", Map.of("Type", "VECTOR",
                                 "VectorKnowledgeBaseConfiguration", Map.of(
-                                        "EmbeddingModelArn", "arn:aws:bedrock:" + this.getRegion() + "::foundation-model/amazon.titan-embed-text-v2:0"
-                                )
-                        ),
-                        "StorageConfiguration", Map.of(
-                                "Type", "OPENSEARCH_SERVERLESS",
+                                        "EmbeddingModelArn", "arn:aws:bedrock:" + this.getRegion() + "::foundation-model/amazon.titan-embed-text-v2:0")),
+                        "StorageConfiguration", Map.of("Type", "OPENSEARCH_SERVERLESS",
                                 "OpensearchServerlessConfiguration", Map.of(
                                         "CollectionArn", ossCollection.getAtt("Arn").toString(),
                                         "VectorIndexName", "runbook-index",
-                                        "FieldMapping", Map.of(
-                                                "VectorField", "embedding",
-                                                "TextField", "text",
-                                                "MetadataField", "metadata"
-                                        )
-                                )
-                        )
-                ))
+                                        "FieldMapping", Map.of("VectorField", "embedding", "TextField", "text", "MetadataField", "metadata")))))
                 .build();
-        knowledgeBase.addDependency(ossCollection);
-        knowledgeBase.addDependency(ossAccessPolicy);
+        knowledgeBase.addDependency(createIndex);
 
-        // Bedrock Knowledge Base Data Source (S3)
         CfnResource dataSource = CfnResource.Builder.create(this, "RunbookDataSource")
                 .type("AWS::Bedrock::DataSource")
-                .properties(Map.of(
-                        "Name", "runbook-s3-source",
+                .properties(Map.of("Name", "runbook-s3-source",
                         "KnowledgeBaseId", knowledgeBase.getAtt("KnowledgeBaseId").toString(),
-                        "DataSourceConfiguration", Map.of(
-                                "Type", "S3",
-                                "S3Configuration", Map.of(
-                                        "BucketArn", runbookBucket.getBucketArn(),
-                                        "InclusionPrefixes", List.of("runbooks/")
-                                )
-                        )
-                ))
+                        "DataSourceConfiguration", Map.of("Type", "S3",
+                                "S3Configuration", Map.of("BucketArn", runbookBucket.getBucketArn(),
+                                        "InclusionPrefixes", List.of("runbooks/")))))
                 .build();
         dataSource.addDependency(knowledgeBase);
 
-        // Outputs
-        new CfnOutput(this, "RunbookBucketName", CfnOutputProps.builder()
-                .value(runbookBucket.getBucketName())
-                .description("S3 bucket for IT runbook documents")
-                .build());
-
-        new CfnOutput(this, "KnowledgeBaseId", CfnOutputProps.builder()
-                .value(knowledgeBase.getAtt("KnowledgeBaseId").toString())
-                .description("Bedrock Knowledge Base ID for application configuration")
-                .build());
-
-        new CfnOutput(this, "DataSourceId", CfnOutputProps.builder()
-                .value(dataSource.getAtt("DataSourceId").toString())
-                .description("Knowledge Base Data Source ID for sync operations")
-                .build());
+        new CfnOutput(this, "RunbookBucketName", CfnOutputProps.builder().value(runbookBucket.getBucketName()).build());
+        new CfnOutput(this, "KnowledgeBaseId", CfnOutputProps.builder().value(knowledgeBase.getAtt("KnowledgeBaseId").toString()).build());
+        new CfnOutput(this, "DataSourceId", CfnOutputProps.builder().value(dataSource.getAtt("DataSourceId").toString()).build());
     }
 
-    public Bucket getRunbookBucket() {
-        return runbookBucket;
+    public Bucket getRunbookBucket() { return runbookBucket; }
+
+    private String getIndexCreatorCode() {
+        return "import json, urllib.request, urllib.error, time, boto3\n" +
+               "from botocore.auth import SigV4Auth\n" +
+               "from botocore.awsrequest import AWSRequest\n" +
+               "import cfnresponse\n\n" +
+               "def handler(event, context):\n" +
+               "    try:\n" +
+               "        if event['RequestType'] == 'Delete':\n" +
+               "            cfnresponse.send(event, context, cfnresponse.SUCCESS, {})\n" +
+               "            return\n" +
+               "        endpoint = event['ResourceProperties']['CollectionEndpoint']\n" +
+               "        index_name = event['ResourceProperties']['IndexName']\n" +
+               "        dimension = int(event['ResourceProperties']['EmbeddingDimension'])\n" +
+               "        print(f'Waiting 60s for collection...')\n" +
+               "        time.sleep(60)\n" +
+               "        url = f'{endpoint}/{index_name}'\n" +
+               "        index_body = json.dumps({'settings':{'index':{'knn':True,'knn.algo_param.ef_search':512}},'mappings':{'properties':{'embedding':{'type':'knn_vector','dimension':dimension,'method':{'engine':'faiss','name':'hnsw','parameters':{'m':16,'ef_construction':512},'space_type':'l2'}},'text':{'type':'text'},'metadata':{'type':'text'}}}})\n" +
+               "        session = boto3.Session()\n" +
+               "        creds = session.get_credentials().get_frozen_credentials()\n" +
+               "        region = session.region_name\n" +
+               "        head_req = AWSRequest(method='HEAD', url=url, headers={'Content-Type':'application/json'})\n" +
+               "        SigV4Auth(creds, 'aoss', region).add_auth(head_req)\n" +
+               "        try:\n" +
+               "            r = urllib.request.Request(url, method='HEAD', headers=dict(head_req.headers))\n" +
+               "            urllib.request.urlopen(r)\n" +
+               "            print(f'Index {index_name} already exists')\n" +
+               "            cfnresponse.send(event, context, cfnresponse.SUCCESS, {'IndexName':index_name})\n" +
+               "            return\n" +
+               "        except urllib.error.HTTPError as e:\n" +
+               "            if e.code != 404: raise\n" +
+               "        put_req = AWSRequest(method='PUT', url=url, data=index_body, headers={'Content-Type':'application/json'})\n" +
+               "        SigV4Auth(creds, 'aoss', region).add_auth(put_req)\n" +
+               "        r = urllib.request.Request(url, data=index_body.encode(), method='PUT', headers=dict(put_req.headers))\n" +
+               "        resp = urllib.request.urlopen(r)\n" +
+               "        print(f'Index created: {json.loads(resp.read().decode())}')\n" +
+               "        cfnresponse.send(event, context, cfnresponse.SUCCESS, {'IndexName':index_name})\n" +
+               "    except Exception as e:\n" +
+               "        print(f'Error: {str(e)}')\n" +
+               "        cfnresponse.send(event, context, cfnresponse.FAILED, {'Error':str(e)})\n";
     }
 }
